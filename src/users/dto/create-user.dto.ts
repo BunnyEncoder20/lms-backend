f8726dc909234ab9b7108e04fcb1e5a9102aba49
Varun Verma <varun.verma2024@gmail.com>
2025-10-09 16:19:03 +0530

    Added createUser and updateUser DTOs

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { Rank, Role } from '@prisma/client';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsEnum(Rank, { message: 'rank must be one of the defined Rank enum values' })
+  rank: Rank;
+
+  @IsEnum(Role, { message: 'role must be one of the defined Role enum values' })
+  role: Role;
+}
