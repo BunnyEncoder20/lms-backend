550dfb658053e8c5d9e0ef16c6d65ed4fb389396
Varun Verma <varun.verma2024@gmail.com>
2025-10-09 12:56:52 +0530

    PrismaORM is finally working with dcoker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@
 FROM node:20-alpine AS development
 WORKDIR /usr/src/app
 COPY package*.json ./
+COPY prisma ./prisma
 RUN npm install
+RUN npx prisma generate
 COPY . .
 EXPOSE 3000
 CMD ["npm", "run", "start:dev"]
@@ -11,7 +13,9 @@ CMD ["npm", "run", "start:dev"]
 FROM node:20-alpine AS build
 WORKDIR /usr/src/app
 COPY package*.json ./
-RUN npm install --only=production
+COPY prisma ./prisma
+RUN npm install
+RUN npx prisma generate
 COPY . .
 RUN npm run build
 
@@ -19,7 +23,7 @@ RUN npm run build
 FROM node:20-alpine AS production
 WORKDIR /usr/src/app
 COPY --from=build /usr/src/app/dist ./dist
+COPY --from=build /usr/src/app/node_modules ./node_modules
 COPY --from=build /usr/src/app/package*.json ./
-RUN npm install --omit=dev
 EXPOSE 3000
 CMD ["npm", "run", "start:prod"]
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'event',
+          level: 'info',
+        },
+        {
+          emit: 'event',
+          level: 'warn',
+        },
+        {
+          emit: 'event',
+          level: 'error',
+        },
+      ],
+    });
+  }
+
+  async onModuleInit() {
+    this.$on('query', (event) => {
+      this.logger.debug(`Query: ${event.query} Params: ${event.params} Duration: ${event.duration}ms`);
+    });
+
+    this.$on('info', (event) => {
+      this.logger.log(event.message);
+    });
+
+    this.$on('warn', (event) => {
+      this.logger.warn(event.message);
+    });
+
+    this.$on('error', (event) => {
+      this.logger.error(event.message);
+    });
+
+    await this.$connect();
+    this.logger.log('Connected to Prisma Database');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('[Prisma] Disconnected from PostgreSQL');
+  }
+}
