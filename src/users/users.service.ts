4b587dd20044e0ff90e5372ab949873ec6f4a0cb
Varun Verma <varun.verma2024@gmail.com>
2025-10-09 16:15:35 +0530

    Created users module, controller and service

---
+++ src/users/users.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async getById(id: number) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+
+  async create(userData: CreateUserDto) {
+    return this.prisma.user.create({
+      data: userData,
+    });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
+  }
+
+  async delete(id: number) {
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
+}
