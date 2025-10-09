4610b96df18f74960273ef5131918dad9e69af11
Varun Verma <varun.verma2024@gmail.com>
2025-10-09 16:34:27 +0530

    removed the prisma module import cause now it's globally injectable

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+// import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  // imports: [PrismaModule],  // cause prismaService is declare with @Global deco, it can be directly injected in all modules
   providers: [UsersService],
   controllers: [UsersController],
 })
