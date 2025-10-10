9e2dfbaf5b5fff3ce0e56ce9be76674973b1cb50
Varun Verma <varun.verma2024@gmail.com>
2025-10-10 11:35:42 +0530

    implemented all basic routes of user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,38 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  /* GET Routes */
   @Get()
-  getAll(@Query('id') id: number) {
-    return this.usersService.getAll(id);
+  getAll() {
+    return this.usersService.getAll();
   }
 
+  @Get(':id')
+  getOne(@Param('id') userId: number) {
+    return this.usersService.getById(userId);
+  }
+
+  /* POST Routes */
   @Post()
-  create(@Body() userData: CreateUserDto) {
-    return this.usersService.create(userData);
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  /* PATCH Routes */
+  @Patch(':id')
+  update(@Param('id') userId: number, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.updateUser(userId, updateUserDto);
+  }
+
+  /* DELETE Routes */
+  @Delete(':id')
+  delete(@Param('id') userId: number) {
+    return this.usersService.delete(userId);
   }
 }
