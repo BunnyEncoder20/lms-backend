e80b0f2443a57de85fd8ab4e0da866fddfcd90c4
Varun Verma <varun.verma2024@gmail.com>
2025-10-10 11:04:34 +0530

    updated users.services. All ts errors handled

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,41 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async getAll(id?: number) {
-    if (id) {
-      return this.prisma.user.findUnique({
-        where: { id },
-      });
-    }
-    return this.prisma.user.findMany();
+  async getAll(): Promise<User[] | null> {
+    return await this.prisma.user.findMany();
   }
 
-  async getById(id: number) {
-    return this.prisma.user.findUnique({
+  async getById(id: number): Promise<User | null> {
+    return await this.prisma.user.findUnique({
       where: { id },
     });
   }
 
-  async create(userData: CreateUserDto) {
-    return this.prisma.user.create({
-      data: userData,
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    return await this.prisma.user.create({
+      data: createUserDto,
     });
   }
 
-  async update(id: number, updateUserDto: UpdateUserDto) {
-    return this.prisma.user.update({
+  async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    return await this.prisma.user.update({
       where: { id },
       data: updateUserDto,
     });
   }
 
-  async delete(id: number) {
-    return this.prisma.user.delete({
+  async delete(id: number): Promise<User> {
+    return await this.prisma.user.delete({
       where: { id },
     });
   }
