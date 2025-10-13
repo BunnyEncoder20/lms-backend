05834f393ab5ad509a753d72c20684aeb577066f
Varun Verma <varun.verma2024@gmail.com>
2025-10-13 14:19:27 +0530

    added import for passport in auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,21 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
+      global: true,
       secret: process.env.JWT_SECRET || 'super-secret-key',
       signOptions: { expiresIn: '1d' },
     }),
+    PassportModule,
+    ConfigModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
