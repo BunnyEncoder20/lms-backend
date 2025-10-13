9378b46118602b2a092f20d71f86972445739f9c
Varun Verma <varun.verma2024@gmail.com>
2025-10-13 14:21:18 +0530

    implemnt jwt-strategy passport strategry

---
+++ src/auth/strategies/jwt.strategies.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    const secret = configService.get<string>('JWT_SECRET'),
+    if (!secret) {
+      throw new Error('JWT_SECRET is not provided in env variables');
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secret,
+    });
+  }
+
+  validate(payload: any) {
+    // the return value gets attached to req.user
+    return {
+      userId: payload.sub,
+      email: payload.email,
+      role: payload.role,
+    };
+  }
+}
