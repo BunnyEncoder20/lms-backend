bacf54842832fbd2acc61eef52f4f343df414076
Varun Verma <varun.verma2024@gmail.com>
2025-10-13 16:42:56 +0530

    changed import of strategy file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './strategies/jwt.strategies';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { ConfigModule } from '@nestjs/config';
 import { PassportAuthController } from './passport-auth.controller';
 
