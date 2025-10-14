e5b48f1c27a9fce673b2b18855523fc5ffeb1c00
Varun Verma <varun.verma2024@gmail.com>
2025-10-14 13:04:39 +0530

    create response-user.dto for user response serialization

---
+++ src/users/dto/response-user.dto.ts
@@ -0,0 +1,20 @@
+import { Exclude } from 'class-transformer';
+
+export class ResponseUserDto {
+  id: number;
+  email: string;
+  role: string;
+
+  @Exclude() password: string;
+  @Exclude() createdAt: Date;
+  @Exclude() updatedAt: Date;
+
+  // If you have relations to other entities, you can include them as well
+  // @Type(() => PostEntity)
+  // @Expose()
+  // posts: PostEntity[];
+
+  constructor(partial: Partial<ResponseUserDto>) {
+    Object.assign(this, partial);
+  }
+}
