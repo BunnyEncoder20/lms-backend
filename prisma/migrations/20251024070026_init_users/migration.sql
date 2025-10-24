061f16328f118185756103cf9b4444162c37cb35
Varun Verma <varun.verma2024@gmail.com>
2025-10-24 12:36:44 +0530

    deleted previous user table schema and migrations. Started fresh

--- prisma/migrations/20251024070026_init_users/migration.sql
+++ prisma/migrations/20251024070026_init_users/migration.sql
@@ -6,16 +6,29 @@ CREATE TYPE "Role" AS ENUM ('SUPER_ADMIN', 'ADMIN', 'TETRA_ADMIN', 'CO', 'JR_CO'
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "personalNumber" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT,
     "rank" "Rank" NOT NULL,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
     "role" "Role" NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "refreshTokenHash" TEXT,
+    "lastLoginAt" TIMESTAMP(3),
+    "failedLoginAttempts" INTEGER NOT NULL DEFAULT 0,
+    "lastPasswordChangeAt" TIMESTAMP(3),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" TEXT,
+    "updatedBy" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "deletedAt" TIMESTAMP(3),
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("personalNumber")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_personalNumber_key" ON "User"("personalNumber");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
