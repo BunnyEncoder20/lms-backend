509e3ba45eb7e5d974a6fd180c6a7a65d2d17b32
Varun Verma <varun.verma2024@gmail.com>
2025-10-27 10:00:14 +0530

    minor improvements to the custom user.decorator

--- src/auth/decorators/user.decorator.ts
+++ src/auth/decorators/user.decorator.ts
@@ -1,21 +1,22 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-import type { Request } from "express";
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import type { Request } from 'express';
 
 /**
  * Custom decorator to extract the user Object or a specific property
  * from the request after it has passed through an AuthGuard
- * eg: @User() 
+ * looks like: @User()
  */
+
 export const User = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext) => {
     // 1. Get the http request obj
     const request = ctx.switchToHttp().getRequest<Request>();
 
-    // 2. The user object was atteached by the JwtStrategy
+    // 2. The user object was atteached by the strategy after validations
     const user = request.user;
 
     // 3. Return a specific property (if data is provided) or the whole user obj
+    // * Bracket notation is Js way of accessing dynamic property names of a obejct (cause the key is a string variable)
     return data ? user?.[data] : user;
-  
-  }
+  },
 );
