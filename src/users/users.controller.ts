9ce937703b20283e80525b6a3dbcb4c977c237b9
Varun Verma <varun.verma2024@gmail.com>
2025-10-27 15:25:05 +0530

    cleaned user.controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,9 @@ import {
   Body,
   Controller,
   Delete,
+  ForbiddenException,
   Get,
+  NotFoundException,
   Param,
   Patch,
   Post,
@@ -18,13 +20,16 @@ import { ResponseUserDto } from './dto/response-user.dto';
 import { JwtAuthGuard } from 'src/auth/guards/passport.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { User } from 'src/auth/decorators/user.decorator';
+import { Role } from '@prisma/client';
+import { ADMIN_ROLES } from 'src/common/constants/app-roles.constant';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   /* GET Routes */
-  @Roles('ADMIN', 'SUPER_ADMIN', 'TETRA_ADMIN', 'CO', 'JR_CO', 'LMS_ADMIN') // RBAC: only ADMIN can access this route
+  @Roles('ADMIN', 'SUPER_ADMIN', 'TETRA_ADMIN', 'LMS_ADMIN') // RBAC: only ADMIN can access this route
   @UseGuards(JwtAuthGuard, RolesGuard) // Protected Route: Need valid JWT token
   @Get()
   async getAll(): Promise<ResponseUserDto[]> {
@@ -37,40 +42,42 @@ export class UsersController {
 
   @UseGuards(JwtAuthGuard)
   @Get('whoami')
-  whoami(@Request() req): ResponseUserDto {
-    return new ResponseUserDto({
-      id: req.user.userId,
-      email: req.user.email,
-      role: req.user.role,
-    });
+  whoami(@User() requser): ResponseUserDto {
+    return new ResponseUserDto(requser);
   }
 
-  @UseGuards(JwtAuthGuard) // Protected Route: Need valid JWT token
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async getOne(
-    @Request() req,
-    @Param('id') userId: string,
+    @User('personalNumber') authPersonalNumber: string,
+    @User('role') authRole: Role,
+    @Param('id') requestedPersonalNumber: string,
+    userId: string,
   ): Promise<ResponseUserDto> {
-    const userData = await this.usersService.getById(parseInt(userId, 10));
+    // 1. Find our user is admin or not
+    const isAdmin = ADMIN_ROLES.includes(authRole);
 
-    // Debugging data
-    // console.log(`token data id:${typeof req.user.userId}:${req.user.userId}`);
-    // console.log(`db data id:${typeof userData.id}:${userData.id}`);
-
-    // User can only access their own data (unless they are an admin)
-    if (
-      userData &&
-      (userData.id === req.user.userId || req.user.role === 'ADMIN')
-    ) {
-      return new ResponseUserDto(userData);
-    } else {
-      throw new UnauthorizedException('User can only access their own data');
+    // 2. Authorization check: Self-Access or Admin-Access
+    if (requestedPersonalNumber !== authPersonalNumber && !isAdmin) {
+      // User not a admin and trying to access another user info
+      throw new ForbiddenException(
+        'You do not have permission to view this user profile',
+      );
     }
+
+    // 3. Data retrival
+    const userData = await this.usersService.getById(requestedPersonalNumber);
+    if (!userData) {
+      throw new NotFoundException();
+    }
+
+    // 4. Serialize and return data
+    return new ResponseUserDto(userData);
   }
 
   /* POST Routes */
+  @Roles('ADMIN', 'SUPER_ADMIN', 'TETRA_ADMIN', 'LMS_ADMIN')
   @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('ADMIN', 'SUPER_ADMIN', 'TETRA_ADMIN', 'CO', 'JR_CO', 'LMS_ADMIN')
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<ResponseUserDto> {
     const results = await this.usersService.createUser(createUserDto);
@@ -78,23 +85,43 @@ export class UsersController {
   }
 
   /* PATCH Routes */
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('ADMIN', 'SUPER_ADMIN', 'TETRA_ADMIN', 'CO', 'JR_CO', 'LMS_ADMIN')
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   async update(
-    @Param('id') userId: number,
+    @User('personalNumber') authPersonalNumber: string,
+    @User('role') authRole: Role,
+    @Param('id') updatePersonalNumber: string,
     @Body() updateUserDto: UpdateUserDto,
   ): Promise<ResponseUserDto> {
-    const results = await this.usersService.updateUser(userId, updateUserDto);
+    // 1. Determine user role
+    const isAdmin = ADMIN_ROLES.includes(authRole);
+
+    // 2. Authorization Check (Self-Update or Admin-Update)
+    if (!isAdmin && authPersonalNumber !== updatePersonalNumber) {
+      // User is not admin and trying to udpate another user's info
+      throw new ForbiddenException(
+        'You do not have permission to update this users profile',
+      );
+    }
+
+    // 3. Data updating
+    const results = await this.usersService.updateUser(
+      updatePersonalNumber,
+      updateUserDto,
+    );
+
+    // 4. Serialization and Return
     return new ResponseUserDto(results);
   }
 
   /* DELETE Routes */
+  @Roles('ADMIN', 'SUPER_ADMIN', 'TETRA_ADMIN', 'LMS_ADMIN')
   @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('ADMIN', 'SUPER_ADMIN', 'TETRA_ADMIN', 'CO', 'JR_CO', 'LMS_ADMIN')
   @Delete(':id')
-  async delete(@Param('id') userId: number): Promise<ResponseUserDto> {
-    const results = await this.usersService.deleteUser(userId);
+  async delete(
+    @Param('id') deletePersonalNumber: string,
+  ): Promise<ResponseUserDto> {
+    const results = await this.usersService.deleteUser(deletePersonalNumber);
     return new ResponseUserDto(results);
   }
 }
